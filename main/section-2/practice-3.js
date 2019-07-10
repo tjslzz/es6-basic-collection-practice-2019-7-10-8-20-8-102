99a5d681b2c035ae9614f58508818740219e8464
JarryLi <3244188372@qq.com>
2019-07-10 22:08:24 +0800

    finish section2 practice-3

--- main/section-2/practice-3.js
+++ main/section-2/practice-3.js
@@ -1,5 +1,30 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result = new Array();
+  var obj = {key:"a", count:0};
+  collection.map((item)=>{
+    if(item.length != 1){
+      if(obj.key == item.substring(0,1)){
+        obj.count = obj.count + parseInt(item.match(/\d+/g));
+      }
+      else{
+        result.push({name:obj.key,summary:obj.count});
+        obj.key = item.substring(0,1);
+        obj.count = parseInt(item.match(/\d+/g));
+      }
+    }
+    else{
+      if(obj.key == item){
+        obj.count = obj.count + 1;
+      }
+      else{
+        result.push({name:obj.key,summary:obj.count});
+        obj.key = item;
+        obj.count = 1;
+      }
+    }
+  });
+  result.push({name:obj.key,summary:obj.count});
+  return result;
 }
