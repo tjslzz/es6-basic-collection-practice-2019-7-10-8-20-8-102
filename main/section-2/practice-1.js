a672b970efc88febb048a07ed13ed64cbe9f0b20
JarryLi <3244188372@qq.com>
2019-07-10 21:58:16 +0800

    finish section-2 paractice-1

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result = new Array();
+  var obj = {key:"a", count:0};
+  collection.map((item)=>{
+    if(obj.key == item){
+      obj.count = obj.count + 1;
+    }
+    else{
+      result.push({key:obj.key,count:obj.count});
+      obj.key = item;
+      obj.count = 1;
+    }
+  });
+  result.push({key:obj.key,count:obj.count});
+  return result;
 }
